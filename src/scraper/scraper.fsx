317b41c5bf003651ca44744a6d1ac6674a6a13d0
Aaron Comyn <aaron@roxor.no>
2016-01-16 21:42:12 +0100

    Initial paket setup

--- src/scraper/scraper.fsx
+++ src/scraper/scraper.fsx
@@ -1,6 +1,38 @@
+(*
+
+    Basic Content Scraper - Youtube
+
+*)
 
 
-(* Basic Content Scraper - Youtube *)
+// Self contained reference management - paket
+
+open System
+open System.IO
+
+Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+
+if not (File.Exists "paket.exe") then
+    let url = "https://github.com/fsprojects/Paket/releases/download/2.44.4/paket.exe"
+    use wc = new Net.WebClient()
+    let tmp = Path.GetTempFileName()
+    wc.DownloadFile(url, tmp);
+    File.Move(tmp,Path.GetFileName url)
+
+#r "paket.exe"
+
+Paket.Scripting.Install """
+    source https://nuget.org/api/v2
+    nuget Suave 0.16.0
+    nuget FSharp.Data
+    nuget FSharp.Charting
+""";;
+
+
+
+
+
+//
 
 
 type MmaSource =
@@ -59,3 +91,11 @@ let HighlightSources : HighlightSource list =
     // (tagged with producer for approval?)
   // Consider alternative hosting solutions here?
     // Essentially watchseries, plus plus
+
+
+
+
+
+
+
+//.
